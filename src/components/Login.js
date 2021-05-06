4f7afdd3252b8cb7e986e94343a8144cb1b263c4
jztimm <jztimm@gmail.com>
2021-05-06 10:57:52 -0400

    Created Login component with custom styling

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,10 +3,32 @@ import styled from 'styled-components'
 
 const Login = () => {
   return (
-    <div>
-      <h2>Creating Disney+ Clone</h2>
-    </div>
+    <Container>
+      <Content>
+        Content
+      </Content>
+    </Container>
   )
 }
+
+const Container = styled.section`
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  height: 100vh
+`
+
+const Content = styled.div`
+  margin-bottom: 10vw;
+  width: 100%;
+  position: relative;
+  min-height: 100vh;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;    // Makes it go to the right 'center'
+  align-items: center;        // Makes items go to the bottom 'center'
+  flex-direction: column;
+`
 
 export default Login
