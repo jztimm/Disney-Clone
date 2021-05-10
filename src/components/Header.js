803d1347370a0dfd75175d5a5e2a01c5953838b1
jztimm <jztimm@gmail.com>
2021-05-10 16:09:40 -0400

    Finished adding remaining navmenu icons and links

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,6 +12,31 @@ const Header = (props) => {
           <img src="/images/home-icon.svg" alt="HOME"/>
           <span>HOME</span>
         </a>
+
+        <a href="/search?">
+          <img src="/images/search-icon.svg" alt="SEARCH"/>
+          <span>SEARCH</span>
+        </a>
+
+        <a href="/watchlist">
+          <img src="/images/watchlist-icon.svg" alt="WATCHLIST"/>
+          <span>WATCHLIST</span>
+        </a>
+
+        <a href="/originals">
+          <img src="/images/original-icon.svg" alt="ORIGINAL"/>
+          <span>ORIGINALS</span>
+        </a>
+
+        <a href="/movies">
+          <img src="/images/movie-icon.svg" alt="MOVIE"/>
+          <span>MOVIES</span>
+        </a>
+
+        <a href="/series">
+          <img src="/images/series-icon.svg" alt="SERIES"/>
+          <span>SERIES</span>
+        </a>
       </NavMenu>
     </Nav>
   )
