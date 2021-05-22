a7f97ccca2c3fcf58cde01b61aca1e7ada60ee4b
jztimm <jztimm@gmail.com>
2021-05-21 22:34:10 -0400

    Add videos to viewers

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -5,18 +5,33 @@ const Viewers = (props) => {
     <Container>
       <Wrap>
         <img src="/images/viewers-disney.png" alt=""/>
+        <video autoPlay={true} loop={true} playsInLine={true}>
+          <source src="/videos/1564674844-disney.mp4" type="video/mp4"/>
+        </video>
       </Wrap>
       <Wrap>
         <img src="/images/viewers-marvel.png" alt=""/>
+        <video autoPlay={true} loop={true} playsInLine={true}>
+          <source src="/videos/1564676115-marvel.mp4" type="video/mp4"/>
+        </video>
       </Wrap>
       <Wrap>
         <img src="/images/viewers-national.png" alt=""/>
+        <video autoPlay={true} loop={true} playsInLine={true}>
+          <source src="/videos/1564676296-national-geographic.mp4" type="video/mp4"/>
+        </video>
       </Wrap>
       <Wrap>
         <img src="/images/viewers-pixar.png" alt=""/>
+        <video autoPlay={true} loop={true} playsInLine={true}>
+          <source src="/videos/1564676714-pixar.mp4" type="video/mp4"/>
+        </video>
       </Wrap>
       <Wrap>
         <img src="/images/viewers-starwars.png" alt=""/>
+        <video autoPlay={true} loop={true} playsInLine={true}>
+          <source src="/videos/1608229455-star-wars.mp4" type="video/mp4"/>
+        </video>
       </Wrap>
     </Container>
   )
@@ -57,6 +72,13 @@ const Wrap = styled.div`
     width: 100%;
     z-index: 1;
     top: 0;
+  }
+
+  video {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0px;
   }
 `
 
